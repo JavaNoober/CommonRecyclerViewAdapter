5a953d96db9c91c23d0a40eafa3d24ae7eba0a76
xiaoqi <812817883@qq.com>
2017-06-16 10:31:04 +0800

    添加对多种布局的支持

--- app/src/main/java/com/xiaoqi/administrator/commonrecyclerviewadapter/CommonRecyclerViewAdapter.java
+++ app/src/main/java/com/xiaoqi/administrator/commonrecyclerviewadapter/CommonRecyclerViewAdapter.java
@@ -22,7 +22,9 @@ public abstract class CommonRecyclerViewAdapter<T> extends RecyclerView.Adapter<
 	private LayoutInflater mInflater;
 	protected Context mContext;
 	private List<T> mData;
-	private final int mItemLayoutId;
+	private int mItemLayoutId;
+	private int[] mItemLayoutIds = new int[]{};
+	private boolean hasDifferentLayouts = false;
 	private OnItemClickListener mOnItemClickListener;
 	private OnItemLongClickListener mOnItemLongClickListener;
 
@@ -30,6 +32,14 @@ public abstract class CommonRecyclerViewAdapter<T> extends RecyclerView.Adapter<
 		this.mContext = context;
 		this.mInflater = LayoutInflater.from(mContext);
 		this.mItemLayoutId = itemLayoutId;
+		this.mData = data;
+	}
+
+	public CommonRecyclerViewAdapter(Context context, List<T> data, int... itemLayoutIds) {
+		hasDifferentLayouts = true;
+		this.mContext = context;
+		this.mInflater = LayoutInflater.from(mContext);
+		this.mItemLayoutIds = itemLayoutIds;
 		this.mData = data;
 	}
 
@@ -43,8 +53,14 @@ public abstract class CommonRecyclerViewAdapter<T> extends RecyclerView.Adapter<
 
 	@Override
 	public RecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-		return new RecyclerViewHolder(mInflater.inflate(mItemLayoutId, parent, false), mOnItemClickListener,
-				mOnItemLongClickListener);
+		if(!hasDifferentLayouts){
+			return new RecyclerViewHolder(mInflater.inflate(mItemLayoutId, parent, false), mOnItemClickListener,
+					mOnItemLongClickListener);
+		}else {
+			return new RecyclerViewHolder(mInflater.inflate(mItemLayoutIds[viewType], parent, false), mOnItemClickListener,
+					mOnItemLongClickListener);
+		}
+
 	}
 
 	@Override
@@ -55,6 +71,14 @@ public abstract class CommonRecyclerViewAdapter<T> extends RecyclerView.Adapter<
 	@Override
 	public int getItemCount(){
 		return mData.size();
+	}
+
+
+	//return的值的顺序必须为mItemLayoutIds中layout的顺序，即
+	//mItemLayoutIds中layout1必须renturn 0;layout2 必须renturn 1;....
+	@Override
+	public int getItemViewType(int position) {
+		return super.getItemViewType(position);
 	}
 
 	public abstract void convert(RecyclerViewHolder holder, T item, int position);
